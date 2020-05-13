acd660ee5547dfc8bd87d2d843724a239d8187c1
Andreibra1 <Andreibr1@users.noreply.github.com>
2020-05-13 12:47:53 -0400

    finalizing Q3

--- COMP348_AS1/schedule-40132881.pl
+++ COMP348_AS1/schedule-40132881.pl
@@ -23,24 +23,24 @@ course_schedule('comp', '352', 'aaae', 'thu', '1215', '1305').
 course_schedule('engr', '202', 'aa', 'mon', '1145', '1300').
 course_schedule('engr', '202', 'aa', 'wed', '1145', '1300').
 course_schedule('engr', '202', 'ab', 'wed', '1015', '1300').
-all_sections(CNAM, CNUM, L) :- findall( L ,takes_course(_, CNAM, CNUM , L), L).
+all_sections(CNAM, CNUM, L) :- findall( X ,takes_course(_, CNAM, CNUM , X), L1), list_to_set(L1, L).
 /* L contains a list of all sections of course CNAME, CNUM,
 i.e. calling all_sections(_, 'comp', '348', L) will result in L=['aa', 'ab'];
 no duplicates */
-has_taken(S, [CNAM|[CNUM|[SEC|[]]]]) :- takes_course(S, CNAM, CNUM, SEC).
+has_taken(S, [CNAM|[CNUM|[SEC|[]]]]) :- takes_course(S, CNAM, CNUM, SEC), !.
 /* true if student S takes the course CNAM CNUM SEC,
 e.g. takes('4000123', ['comp', '348', 'aa']) */
-has_taken2(S, [CNAM|[CNUM|[]]]) :- takes_course(S, CNAM, CNUM, _).
+has_taken2(S, [CNAM|[CNUM|[]]]) :- takes_course(S, CNAM, CNUM, _), !.
 /* true if S takes any sections of the course CNAM CNUM,
 e.g. takes('4000123', ['comp', '348']) */
 all_subjects(S, L) :-takes_course(S, L, _, _).
 /* L contains all the courses subjects that have been
 taken by student S, i.e. ['comp', 'soen']; no duplicates */
-all_courses(S, L) :- takes_course(S, L). /*No work*/
+all_courses(S, L) :- findall([X, Y, Z], takes_course(S, X, Y, Z), L).
 /* L contains all the courses that have been taken by
 student S, i.e. all_courses('4000123', L) will result in
 L=[['comp', '348', 'aa'], ['comp', '348', 'ab']] */
-all_courses2(S, L) :- ...
+all_courses2(S, L) :- findall([X, Y], takes_course(S, X, Y, _), L1), list_to_set(L1, L).
 /* similar to all_courses but without section info;
 no duplicates */
 
