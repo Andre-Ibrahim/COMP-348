e854faef2404f5b697fb13ec0a2e233227f1c039
Alexandru Bara <alexandrubara2000@gmail.com>
2020-05-25 17:01:10 -0400

    Q7 progress

---
+++ COMP348_AS2/Question7.lisp
@@ -0,0 +1,31 @@
+
+(defun collatz (n)
+(setq a n)
+(setq li (list a))
+(loop 
+    (if (oddp a) 
+        (setq a (+ (* a 3) 1))
+        (setq a (/ a 2))
+    )
+   (setq li (append li (list a)))
+   (when (< a 2) (return a))
+)
+(cdr li)
+)
+
+(print (collatz 30))
+
+
+
+(defun collatz2 (n)
+    (setq b 0)
+    (loop 
+        (setq b (+ b 1))
+        (print (collatz b))
+        
+   (when (> b n) (return b))
+)
+
+)
+
+(print (collatz2 20))
