f1edfa7f71c8476a7aed861f8ba1a99a0055d3e9
Alexandru Bara <alexandrubara2000@gmail.com>
2020-06-13 10:58:58 -0400

    q4 done

--- COMP348_AS3/Ruby_Q4.rb
+++ COMP348_AS3/Ruby_Q4.rb
@@ -1,3 +1,5 @@
+include Math
+
 class Shape
     attr_accessor :area, :name, :perimeter
     def initialize()
@@ -23,25 +25,77 @@ class Circle < Shape
         super()
         @radius=radius
         @name="circle"
-        @area= @radius*3
-        @perimeter= @radius*2
+        @area= @radius**2 *Math::PI
+        @perimeter= @radius*2*Math::PI
     end
     
-    def print()
-        puts "The name of the shape is: "+@name+". The area is: "+@area.to_s+". The perimeter is: "+      @perimeter.to_s+"."
-    end
     def perimeter()
-        return nil
+        return @perimeter
     end
     def area()
-        return nil
+        return @area
     end
 end
+
+class Rectangle < Shape
+    attr_accessor :height, :width
+    def initialize(height, width)
+        super()
+        @height = height
+        @width = width
+        @name="rectangle"
+        @area= @height * @width
+        @perimeter= 2*@height + 2*@width
+    end
+    
+    def perimeter()
+        return @perimeter
+    end
+    def area()
+        return @area
+    end
+end
+
+class Elipse < Shape
+    attr_accessor :a, :b
+    def initialize(a, b)
+        super()
+        @a = a
+        @b = b
+        @name="elipse"
+        @area= @a * @a * Math::PI
+        @eccentricity = Math.sqrt(a**2 + b**2)
+    end
+    
+    def perimeter()
+        return @perimeter
+    end
+    def area()
+        return @area
+    end
+    def eccentricity()
+        return @eccentricity
+    end
+end
+
 
 x = Shape.new()
 x.print
 puts x.perimeter()
-x = Circle.new(5)
-x.print
-puts x.perimeter()
+puts x.area()
+u = Circle.new(5)
+u.print
+puts u.perimeter()
+puts u.area()
+r = Rectangle.new(5, 4)
+r.print
+puts r.perimeter()
+puts r.area()
+e = Elipse.new(3, 4)
+e.print
+puts e.perimeter()
+puts e.area()
+puts e.eccentricity
+
+
 
